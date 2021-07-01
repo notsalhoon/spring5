151c31e7e7246d8986f1cfeb535ab56d2fee9b35
notsalhoon <keroro10ha@naver.com>
2021-07-01 16:01:41 +0900

    chap06

---
+++ sp5-chap06/src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import config.AppCtx;
+import spring.Client;
+
+public class Main {
+
+	public static void main(String[] arg) throws IOException {
+		AbstractApplicationContext ctx =
+				new AnnotationConfigApplicationContext(AppCtx.class);
+		
+		Client client= ctx.getBean(Client.class);
+		client.send();
+		
+		ctx.close();
+	}
+}
